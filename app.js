4e7357c125240c34d4ffae42ff5a188dc170fec9
Tereza Gonsiorova <sgtgonsi@liverpool.ac.uk>
2024-05-13 21:47:43 +0200

    fixes of enable/disable course
    checks that course exists

--- app.js
+++ app.js
@@ -33,23 +33,37 @@ app.put('/courses/:id/enable/:userId', (req, res) => {
     const courseId = req.params.id; // Extract courseId from request parameters
     const userId = req.params.userId; // Extract userId from request parameters
 
+    // Check if course exists
+
+    const doesCourseExistPromise = does_course_exist(courseId)
+
     // Check if the user is an admin
-    is_admin(userId)
-        .then((isAdmin) => {
-            if (!isAdmin) {
-                return res.status(403).json({ error: 'Unauthorized' }); // Send 403 Forbidden if user is not authorized
+
+    const isAdminPromise = is_admin(userId)
+
+    Promise.all([doesCourseExistPromise, isAdminPromise]).then((values) => {
+
+        doesCourseExist = values[0]
+        isAdmin = values[1]
+
+        if (!isAdmin) {
+            return res.status(403).json({ error: 'Unauthorized' }); // Send 403 Forbidden if user is not authorized
+        }
+
+        if (!doesCourseExist) {
+            return res.status(403).json({ error: 'Course does not exist' }); // Send 403 Forbidden if course does not exist 
+        }
+
+        // Main SQL query to update the database
+        connection.query('UPDATE courses SET isAvailable = 1 WHERE CourseID = ?', [courseId], (updateError, updateResults) => {
+            if (updateError) {
+                console.error('Error enabling course:', updateError);
+                return res.status(500).json({ error: 'An error occurred while enabling the course' }); // Send 500 Internal Server Error if there's an SQL error
             }
 
-            // Main SQL query to update the database
-            connection.query('UPDATE courses SET isAvailable = 1 WHERE CourseID = ?', [courseId], (updateError, updateResults) => {
-                if (updateError) {
-                    console.error('Error enabling course:', updateError);
-                    return res.status(500).json({ error: 'An error occurred while enabling the course' }); // Send 500 Internal Server Error if there's an SQL error
-                }
-
-                res.json({ message: 'Course enabled successfully' }); // Confirmation if the course is enabled successfully
-            });
-        })
+            res.json({ message: 'Course enabled successfully' }); // Confirmation if the course is enabled successfully
+        });
+    })
         .catch((error) => {
             return res.status(500).json({ error: error }); // Send 500 Internal Server Error if there's an SQL error
         });
@@ -60,23 +74,36 @@ app.put('/courses/:id/disable/:userId', (req, res) => {
     const courseId = req.params.id; // Extract courseId from request parameters
     const userId = req.params.userId; // Extract userId from request parameters
 
+    // Check if course exists
+
+    const doesCourseExistPromise = does_course_exist(courseId)
+
     // Check if the user is an admin
-    is_admin(userId)
-        .then((isAdmin) => {
-            if (!isAdmin) {
-                return res.status(403).json({ error: 'Unauthorized' }); // Send 403 Forbidden if user is not authorized
+
+    const isAdminPromise = is_admin(userId)
+
+    Promise.all([doesCourseExistPromise, isAdminPromise]).then((values) => {
+
+        doesCourseExist = values[0]
+        isAdmin = values[1]
+        if (!isAdmin) {
+            return res.status(403).json({ error: 'Unauthorized' }); // Send 403 Forbidden if user is not authorized
+        }
+
+        if (!doesCourseExist) {
+            return res.status(403).json({ error: 'Course does not exist' }); // Send 403 Forbidden if course does not exist 
+        }
+
+        // Main SQL query to update the database
+        connection.query('UPDATE courses SET isAvailable = 0 WHERE CourseID = ?', [courseId], (updateError, updateResults) => {
+            if (updateError) {
+                console.error('Error disabling course:', updateError);
+                return res.status(500).json({ error: 'An error occurred while disabling the course' }); // Send 500 Internal Server Error if there's an SQL error
             }
 
-            // Main SQL query to update the database
-            connection.query('UPDATE courses SET isAvailable = 0 WHERE CourseID = ?', [courseId], (updateError, updateResults) => {
-                if (updateError) {
-                    console.error('Error disabling course:', updateError);
-                    return res.status(500).json({ error: 'An error occurred while disabling the course' }); // Send 500 Internal Server Error if there's an SQL error
-                }
-
-                res.json({ message: 'Course disabled successfully' }); // Confirmation if the course is disabled successfully
-            });
-        })
+            res.json({ message: 'Course disabled successfully' }); // Confirmation if the course is disabled successfully
+        });
+    })
         .catch((error) => {
             return res.status(500).json({ error: error }); // Send 500 Internal Server Error if there's an SQL error
         });
@@ -87,16 +114,15 @@ app.put('/courses/:id/assign_teacher/:teacherId/:userId', (req, res) => {
     const courseId = req.params.id; // Extract courseId from request parameters
     const teacherId = req.params.teacherId; // Exctract teacherId from request parameters
     const userId = req.params.userId; // Extract userId from request parameters   
-    
+
     // Check if user that is being assigned has the role teacher via a promise
     const isTeacherPromise = is_teacher(teacherId)
-    
+
     // Check that the current user is an admin via a promise
     const isAdminPromise = is_admin(userId)
 
     // Callback for all promises
     Promise.all([isTeacherPromise, isAdminPromise]).then((values) => {
-        console.log(values)
         isTeacher = values[0]
         isAdmin = values[1]
         if (!isAdmin) {
@@ -113,10 +139,10 @@ app.put('/courses/:id/assign_teacher/:teacherId/:userId', (req, res) => {
 
             res.json({ message: 'Teacher assigned successfully' }); // Confirmation if the course is enabled successfully
         });
-      }).catch((error) => {
+    }).catch((error) => {
         return res.status(500).json({ error: error }); // Send 500 Internal Server Error if there's an SQL error
     });
-   
+
 });
 
 // Define route handler for listing available courses with course title and teacher's name
@@ -128,7 +154,7 @@ app.get('/courses', (req, res) => {
         JOIN users ON courses.TeacherID = users.UserID 
         WHERE courses.isAvailable = 1;
     `;
-    
+
     // Execute the SQL query
     connection.query(query, (error, results) => {
         if (error) {
@@ -146,7 +172,7 @@ app.get('/courses', (req, res) => {
 app.put('/courses/:id/enrol_student/:userId', (req, res) => {
     const courseId = req.params.id; // Extract courseId from request parameters
     const userId = req.params.userId; // Extract userId from request parameters   
-    
+
     // Check if the user is a student
     is_student(userId)
         .then((isStudent) => {
@@ -187,18 +213,17 @@ app.put('/courses/:id/enrol_student/:userId', (req, res) => {
 app.put('/courses/:id/set_grade/:studentId/:passed/:userId', (req, res) => {
     const courseId = req.params.id; // Extract courseId from request parameters
     const studentId = req.params.studentId; //Extract studentId from request parameters
-    const passed = parseInt (req.params.passed); // Extract passed from request parameters
+    const passed = parseInt(req.params.passed); // Extract passed from request parameters
     const userId = req.params.userId; // Extract userId from request parameters   
-    
+
     // Check if user that is being assigned has the role teacher via a promise
     const isTeacherPromise = is_teacher(userId)
-    
+
     // Check if user that is being assigned has the role student via a promise
     const isStudentPromise = is_student(studentId)
 
     // Callback for all promises
     Promise.all([isTeacherPromise, isStudentPromise]).then((values) => {
-        console.log(values)
         isTeacher = values[0]
         isStudent = values[1]
 
@@ -212,22 +237,22 @@ app.put('/courses/:id/set_grade/:studentId/:passed/:userId', (req, res) => {
 
         // Checking if passed (0 or 1)
         if (!(passed === 0 || passed === 1)) {
-            return res.status(403).json({ error: "passed should be 1 or 0"}) //Error that passed should be 1 or 0
+            return res.status(403).json({ error: "passed should be 1 or 0" }) //Error that passed should be 1 or 0
         }
 
-        connection.query('UPDATE enrolments SET Mark = ? WHERE CourseID = ? AND UserID = ?', 
-        [passed, courseId, studentId], (updateError, updateResults) => {
-            if (updateError) {
-                console.error('Error assigning grade:', updateError);
-                return res.status(500).json({ error: 'An error occurred while assigning grade to the student' }); // Send 500 Internal Server Error if there's an SQL error
-            }
+        connection.query('UPDATE enrolments SET Mark = ? WHERE CourseID = ? AND UserID = ?',
+            [passed, courseId, studentId], (updateError, updateResults) => {
+                if (updateError) {
+                    console.error('Error assigning grade:', updateError);
+                    return res.status(500).json({ error: 'An error occurred while assigning grade to the student' }); // Send 500 Internal Server Error if there's an SQL error
+                }
 
-            res.json({ message: 'Grade assigned successfully' }); // Confirmation if the grade is assigned successfully
-        });
-      }).catch((error) => {
+                res.json({ message: 'Grade assigned successfully' }); // Confirmation if the grade is assigned successfully
+            });
+    }).catch((error) => {
         return res.status(500).json({ error: error }); // Send 500 Internal Server Error if there's an SQL error
     });
-   
+
 });
 
 
@@ -282,3 +307,20 @@ function is_student(userId) {
     });
 }
 
+// Function to check if course exists
+
+function does_course_exist(courseId) {
+    return new Promise((resolve, reject) => {
+        connection.query('SELECT * FROM courses WHERE CourseID = ?', [courseId], (error, results) => {
+            if (error) {
+                console.error('Error fetching course details:', error);
+                reject('An error occurred while fetching course details')
+            }
+            if (results.length === 0) {
+                resolve(false)
+            } else {
+                resolve(true)
+            }
+        });
+    });
+}
