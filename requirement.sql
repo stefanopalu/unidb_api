fac7ff1eb3388e53690ff9ec048740f5ecee3844
Stefano Palumbo <stefanizzimail@hotmail.it>
2024-05-17 15:06:04 +0900

    updated sql queries file

--- requirement.sql
+++ requirement.sql
@@ -1,36 +1,46 @@
 -- 1) Admins should be able to enable or disable the availability of a course
-UPDATE courses SET isAvailable = 1 WHERE CourseID = 5;
-UPDATE courses SET isAvailable = 0 WHERE CourseID = 5;
+-- Confirm if the course exist and the user is an admin
+SELECT * FROM courses WHERE CourseID = ?
+SELECT * FROM users JOIN roles ON roles.RoleID = users.RoleID WHERE Role = "Admin" AND UserID = ?
+
+-- Update the course table
+UPDATE courses SET isAvailable = 1 WHERE CourseID = ?;
+UPDATE courses SET isAvailable = 0 WHERE CourseID = ?;
+
 -- 2) Admins should be able to assign one or more courses to a teacher
--- Find all users that are teachers
-SELECT * FROM users JOIN roles ON roles.RoleID = users.RoleID WHERE Role = "Teacher";
-UPDATE courses SET TeacherID = 3 WHERE CourseID = 5;
--- 3) Students can browse and list all the available courses and see the course title and course
--- teacher’s name.
+-- Confirm if the course exist, if the teacher id is valid and if the user is an admin
+SELECT * FROM courses WHERE CourseID = ?
+SELECT * FROM users JOIN roles ON roles.RoleID = users.RoleID WHERE Role = "Admin" AND UserID = ?
+SELECT * FROM users JOIN roles ON roles.RoleID = users.RoleID WHERE Role = "Teacher" AND UserID = ?
+
+-- Update the course table
+UPDATE courses SET TeacherID = ? WHERE CourseID = ?
+
+
+-- 3) Students can browse and list all the available courses and see the course title and course teacher’s name.
 SELECT courses.Title, users.Name AS TeacherName 
 FROM courses 
-JOIN users ON courses.TeacherID = users.UserID 
-WHERE courses.isAvailable = 1; 
--- 4) Students can enrol in a course. Students should not be able to enrol in a course more than
--- once at each time.
--- Find all users that are students
-INSERT INTO enrolments (CourseID, UserID)
-SELECT 1, 10
-FROM enrolments
-WHERE NOT EXISTS (
-    SELECT *
-    FROM enrolments
-    WHERE CourseID = 1 AND UserID = 10
-    LIMIT 1
-);
--- 5) Teachers can fail or pass a student.
-UPDATE enrolments SET Mark = 1 WHERE CourseID =5 AND UserID = 10;
--- 6) Access control for Admins, Teachers and Students: Ensure only the authorized access can
--- perform an action. For example, only teachers can pass/fail a student.
--- Is user a student?
-SELECT * FROM users JOIN roles ON roles.RoleID = users.RoleID WHERE Role = "Student" AND UserID = 10;
--- Is user an admin?
-SELECT * FROM users JOIN roles ON roles.RoleID = users.RoleID WHERE Role = "Admin" AND UserID = 10;
--- Is user a teacher?
-SELECT * FROM users JOIN roles ON roles.RoleID = users.RoleID WHERE Role = "Teacher" AND UserID = 10;
+LEFT JOIN users ON courses.TeacherID = users.UserID 
+WHERE courses.isAvailable = 1;
 
+
+-- 4) Students can enrol in a course. Students should not be able to enrol in a course more than once at each time.
+-- Confirm if the course is available and the user is a student
+SELECT * FROM users JOIN roles ON roles.RoleID = users.RoleID WHERE Role = "Student" AND UserID = ?SELECT 1, 10
+SELECT isAvailable FROM courses WHERE courseId = ?
+
+-- Check if the record is already in the enrolments table
+SELECT * FROM enrolments WHERE CourseID = ? AND UserID = ?
+
+-- Update the enrolments table
+INSERT INTO enrolments (CourseID, UserID) VALUES (?, ?)
+
+
+-- 5) Teachers can fail or pass a student.
+-- Confirm if the course exist, the student ID is valid and if the user is the teacher assigned to the course
+SELECT * FROM courses WHERE CourseID = ?
+SELECT * FROM users JOIN roles ON roles.RoleID = users.RoleID WHERE Role = "Student" AND UserID = ?
+SELECT TeacherID FROM courses WHERE CourseID = ? AND TeacherID = ?
+
+-- Update the enrolments table
+UPDATE enrolments SET Mark = ? WHERE CourseID = ? AND UserID = ?
